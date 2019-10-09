81073bffff6d64c238ed0d697dd5324c4f7128aa
domenicostefani <domenico.stefani96@gmail.com>
2019-10-09 09:51:40 +0200

    add topic to saved image and create folder command

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -3,59 +3,69 @@
 
 #include <stdexcept>
 #include <sstream>
+#include <stdlib.h>
 namespace student {
- int i = 0; 
+ int i = 0;
 
- void loadImage(cv::Mat& img_out, const std::string& config_folder){  
+ void loadImage(cv::Mat& img_out, const std::string& config_folder){
    throw std::logic_error( "STUDENT FUNCTION - LOAD IMAGE - NOT IMPLEMENTED" );
  }
 
  void genericImageListener(const cv::Mat& img_in, std::string topic, const std::string& config_folder){
     //throw std::logic_error( "STUDENT FUNCTION - IMAGE LISTENER - NOT CORRECTLY IMPLEMENTED" );
-    
+
+
     cv::imshow("current picture", img_in);
     char c = cv::waitKey(30);
     if(c == 's'){
-        std::string filename = config_folder;
-        filename += "/image_";
-        filename += std::to_string(i++);
-        filename += ".jpg";
-        cv::imwrite(filename, img_in);
+      /* Create folder for images*/
+      std::string foldername = config_folder;
+      foldername += "/image";      //This is the folder used in the topic string
+      std::string command = "mkdir -p " + foldername;  //-p creates only if non exists
+      system(command.c_str());  //use bash command
+
+      /* Save current image */
+      std::string filename = config_folder;
+      filename += topic;
+      filename += "_";
+      filename += std::to_string(i++);
+      filename += ".jpg";
+      cv::imwrite(filename, img_in);
+      printf("Saved is '%s'\n", filename.c_str());
     }
   }
 
   bool extrinsicCalib(const cv::Mat& img_in, std::vector<cv::Point3f> object_points, const cv::Mat& camera_matrix, cv::Mat& rvec, cv::Mat& tvec, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - EXTRINSIC CALIB - NOT IMPLEMENTED" );   
+    throw std::logic_error( "STUDENT FUNCTION - EXTRINSIC CALIB - NOT IMPLEMENTED" );
   }
 
-  void imageUndistort(const cv::Mat& img_in, cv::Mat& img_out, 
+  void imageUndistort(const cv::Mat& img_in, cv::Mat& img_out,
           const cv::Mat& cam_matrix, const cv::Mat& dist_coeffs, const std::string& config_folder){
 
-    throw std::logic_error( "STUDENT FUNCTION - IMAGE UNDISTORT - NOT IMPLEMENTED" );  
+    throw std::logic_error( "STUDENT FUNCTION - IMAGE UNDISTORT - NOT IMPLEMENTED" );
 
   }
 
   void findPlaneTransform(const cv::Mat& cam_matrix, const cv::Mat& rvec, const cv::Mat& tvec, const std::vector<cv::Point3f>& object_points_plane, cv::Mat& plane_transf, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - FIND PLANE TRANSFORM - NOT IMPLEMENTED" );  
+    throw std::logic_error( "STUDENT FUNCTION - FIND PLANE TRANSFORM - NOT IMPLEMENTED" );
   }
 
 
   void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf, const double scale, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - UNWRAP - NOT IMPLEMENTED" );   
+    throw std::logic_error( "STUDENT FUNCTION - UNWRAP - NOT IMPLEMENTED" );
   }
 
   bool processMap(const cv::Mat& img_in, const double scale, std::vector<Polygon>& obstacle_list, std::vector<std::pair<int,Polygon>>& victim_list, Polygon& gate, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - PROCESS MAP - NOT IMPLEMENTED" );   
+    throw std::logic_error( "STUDENT FUNCTION - PROCESS MAP - NOT IMPLEMENTED" );
   }
 
   bool findRobot(const cv::Mat& img_in, const double scale, Polygon& triangle, double& x, double& y, double& theta, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - FIND ROBOT - NOT IMPLEMENTED" );    
+    throw std::logic_error( "STUDENT FUNCTION - FIND ROBOT - NOT IMPLEMENTED" );
   }
 
   bool planPath(const Polygon& borders, const std::vector<Polygon>& obstacle_list, const std::vector<std::pair<int,Polygon>>& victim_list, const Polygon& gate, const float x, const float y, const float theta, Path& path){
-    throw std::logic_error( "STUDENT FUNCTION - PLAN PATH - NOT IMPLEMENTED" );     
+    throw std::logic_error( "STUDENT FUNCTION - PLAN PATH - NOT IMPLEMENTED" );
   }
 
 
 }
-
