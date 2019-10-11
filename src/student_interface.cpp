6566add222d6c7b00c15ef5496b4e6bc25e9e74d
domenicostefani <domenico.stefani96@gmail.com>
2019-10-11 10:56:03 +0200

    partial commit to test lab pc

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -163,13 +163,79 @@ namespace student {
     plane_transf = cv::getPerspectiveTransform(image_points, dest_image_points_plane);
   }
 
-  void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf, const std::string& config_folder){
+  void unwarp(const cv::Mat& img_in, cv::Mat& img_out,
+              const cv::Mat& transf, const std::string& config_folder){
     // throw std::logic_error( "STUDENT FUNCTION - UNWRAP - NOT IMPLEMENTED" );
     cv::warpPerspective(img_in, img_out, transf, img_in.size());
   }
 
-  bool processMap(const cv::Mat& img_in, const double scale, std::vector<Polygon>& obstacle_list, std::vector<std::pair<int,Polygon>>& victim_list, Polygon& gate, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - PROCESS MAP - NOT IMPLEMENTED" );
+  void findObstacles(const cv::Mat& hsv_img, const double scale,
+                        std::vector<Polygon>& obstacle_list){
+     //Obstacles are red
+
+     /* Red color requires 2 ranges*/
+     cv::Mat lower_red_hue_range; // the lower range for red hue
+     cv::Mat upper_red_hue_range; // the higher range for red hue
+     cv::inRange(hsv_img, cv::Scalar(0, 100, 100), cv::Scalar(10, 255, 255), lower_red_hue_range);
+     cv::inRange(hsv_img, cv::Scalar(160, 100, 100), cv::Scalar(179, 255, 255), upper_red_hue_range);
+     // Now we can combine the 2 masks
+     cv::Mat red_hue_image;
+     cv::addWeighted(lower_red_hue_range, 1.0, upper_red_hue_range, 1.0, 0.0, red_hue_image);
+     //This can reduce false positives
+     // cv::GaussianBlur(red_hue_image, red_hue_image, cv::Size(9, 9), 2, 2);
+
+     imshow("Red filtered",red_hue_image);
+     cv::waitKey(0);
+
+     /*
+      * Now the mask has to undergo contour detection
+     */
+
+     std::vector<std::vector<cv::Point>> contours, contours_approx;
+     std::vector<cv::Point> approx_curve;
+     cv::Mat contours_img;
+
+     cv::findContours(red_hue_image, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+     drawContours(contours_img, contours, -1, cv::Scalar(40,190,40), 1, cv::LINE_AA); //REMOVE
+     for (int i=0; i<contours.size(); ++i)
+     {
+       approxPolyDP(contours[i], approx_curve, 3, true);
+
+       Polygon scaled_contour;
+       for (const auto& pt: approx_curve) {
+         scaled_contour.emplace_back(pt.x/scale, pt.y/scale);
+       }
+       obstacle_list.push_back(scaled_contour);
+    }
+  }
+
+  bool processMap(const cv::Mat& img_in, const double scale,
+                  std::vector<Polygon>& obstacle_list,
+                  std::vector<std::pair<int,Polygon>>& victim_list,
+                  Polygon& gate, const std::string& config_folder){
+
+      // imshow("Detect here",img_in);
+      // cv::waitKey(0);
+
+      // Convert to HSV for better color detection
+      cv::Mat img_hsv;
+      cv::cvtColor(img_in, img_hsv, cv::COLOR_BGR2HSV);
+
+      findObstacles(img_hsv, scale, obstacle_list);
+      // findGate(img_hsv, scale, gate);
+      // findVictims(img_hsv, scale, victim_list);
+
+
+      // contours_img = hsv_img.clone();
+      // for(Polygon *contour:obstacle_list){
+      //   contours_approx = {approx_curve};
+      //   drawContours(contours_img, contours_approx, -1, cv::Scalar(0,170,220), 3, cv::LINE_AA);
+      // }
+      // imshow("Contours", contours_img);
+      // cv::waitKey(0);
+
+      return true;
   }
 
   bool findRobot(const cv::Mat& img_in, const double scale, Polygon& triangle, double& x, double& y, double& theta, const std::string& config_folder){
