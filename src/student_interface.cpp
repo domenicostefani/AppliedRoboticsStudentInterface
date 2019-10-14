a32b40b832729ccfa672874a2feaec3cf91b147c
lar2019 <caterina.battisti@studenti.unitn.it>
2019-10-14 15:27:42 +0200

    add findRobot base function and loadImage

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -11,7 +11,44 @@ namespace student {
  int i = 0;
 
  void loadImage(cv::Mat& img_out, const std::string& config_folder){
-   throw std::logic_error( "STUDENT FUNCTION - LOAD IMAGE - NOT IMPLEMENTED" );
+    //throw std::logic_error( "STUDENT FUNCTION - LOAD IMAGE - NOT IMPLEMENTED" );
+
+    static bool initialized = false;
+    static std::vector<cv::String> img_list; // list of images to load
+    static size_t idx = 0;  // idx of the current img
+    static size_t function_call_counter = 0;  // idx of the current img
+    const static size_t freeze_img_n_step = 30; // hold the current image for n iteration
+    static cv::Mat current_img; // store the image for a period, avoid to load it from file every time
+
+    if(!initialized){
+        const bool recursive = false;
+        // Load the list of jpg image contained in the config_folder/img_to_load/
+        cv::glob(config_folder + "/img_to_load/*.jpg", img_list, recursive);
+
+        if(img_list.size() > 0){
+          initialized = true;
+          idx = 0;
+          current_img = cv::imread(img_list[idx]);
+          function_call_counter = 0;
+        }else{
+          initialized = false;
+        }
+    }
+
+    if(!initialized){
+        throw std::logic_error( "Load Image can not find any jpg image in: " +  config_folder + "/img_to_load/");
+        return;
+    }
+
+    img_out = current_img;
+    function_call_counter++;  
+
+    // If the function is called more than N times load increment image idx
+    if(function_call_counter > freeze_img_n_step){
+        function_call_counter = 0;
+        idx = (idx + 1)%img_list.size();    
+        current_img = cv::imread(img_list[idx]);
+    }
  }
 
  void genericImageListener(const cv::Mat& img_in, std::string topic, const std::string& config_folder){
@@ -239,7 +276,121 @@ namespace student {
   }
 
   bool findRobot(const cv::Mat& img_in, const double scale, Polygon& triangle, double& x, double& y, double& theta, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - FIND ROBOT - NOT IMPLEMENTED" );
+    //throw std::logic_error( "STUDENT FUNCTION - FIND ROBOT - NOT IMPLEMENTED" );
+    #define FIND_ROBOT_DEBUG_PLOT
+
+    // Convert color space from BGR to HSV
+    cv::Mat hsv_img;
+    cv::cvtColor(img_in, hsv_img, cv::COLOR_BGR2HSV);
+
+    // Extract blue color region
+    cv::Mat blue_mask;    
+    cv::inRange(hsv_img, cv::Scalar(100, 120, 150), 
+    cv::Scalar(135, 255, 255), blue_mask);
+
+    // remove noise with opening operation
+    cv::morphologyEx(blue_mask, blue_mask, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5,5)));
+
+    // find robot contours and approximate to triangle
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(blue_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    #ifdef FIND_ROBOT_DEBUG_PLOT // do this only if FIND_DEBUG_PLOT is defined
+      //cv::imshow("findRobotHsv", hsv_img);
+      //cv::imshow("findRobotMask", blue_mask);
+      cv::Mat contours_img;
+      contours_img = img_in.clone();
+      //cv::drawContours(contours_img, contours, -1, cv::Scalar(0,0,0), 4, cv::LINE_AA);
+      std::cout << "Number of contours: " << contours.size() << std::endl;
+    #endif
+
+    std::vector<cv::Point> approx_curve;
+    std::vector<std::vector<cv::Point>> contours_approx;    
+    bool found = false;
+    for (int i=0; i<contours.size(); ++i)
+    { 
+        // Approximate the i-th contours      
+        cv::approxPolyDP(contours[i], approx_curve, 30, true);
+
+        // Check the number of edge of the aproximated contour
+        if (approx_curve.size() != 3) continue;
+
+        // draw approximated contours
+        #ifdef FIND_ROBOT_DEBUG_PLOT
+            contours_approx = {approx_curve};
+            cv::drawContours(contours_img, contours_approx, -1, cv::Scalar(0,0,255), 1, cv::LINE_AA);
+        #endif
+
+        cv::imshow("findRobot", contours_img);
+        cv::waitKey(0);
+
+        found = true;
+        break;
+    }
+
+    // set robot position
+    if (found){      
+        // emplace back every vertex on triangle (output of this function)
+        for (const auto& pt: approx_curve) {        
+          triangle.emplace_back(pt.x/scale, pt.y/scale);
+          // remember to use the scale to convert the position on the image
+          // (pixels) to the position in the arena (meters)
+        }
+
+        // Find the position of the robot (baricenter of the triangle)
+        double cx = 0, cy = 0;
+
+        // Compute the triangle baricenter
+        for (auto vertex: triangle) 
+        {
+          // NB: triangle point are expressed in meters
+          cx += vertex.x;
+          cy += vertex.y;
+        }
+        cx /= static_cast<double>(triangle.size());
+        cy /=  static_cast<double>(triangle.size());
+
+        // Find the robot orientation (i.e the angle of height relative to the base with the x axis)
+        double dst = 0;
+        Point top_vertex;
+        for (auto& vertex: triangle)
+        {
+          const double dx = vertex.x-cx;      
+          const double dy = vertex.y-cy;
+          const double curr_d = dx*dx + dy*dy;
+          if (curr_d > dst)
+          { 
+            dst = curr_d;
+            top_vertex = vertex;
+          }
+        }
+
+        // Store the position of the robot in the output
+        x = cx;
+        y = cy;
+
+        // Compute the robot orientation
+        const double dx = cx - top_vertex.x;
+        const double dy = cy - top_vertex.y;
+        theta = std::atan2(dy, dx);
+        
+        #ifdef FIND_ROBOT_DEBUG_PLOT   
+            // Draw over the image
+            cv::Point cv_baricenter(x*scale, y*scale); // convert back m to px
+            cv::Point cv_vertex(top_vertex.x*scale, top_vertex.y*scale); // convert back m to px
+            cv::line(contours_img, cv_baricenter, cv_vertex, cv::Scalar(0,255,0), 3);
+            cv::circle(contours_img, cv_baricenter, 5, cv::Scalar(0,0,255), -1);
+            cv::circle(contours_img, cv_vertex, 5, cv::Scalar(0,255,0), -1);      
+            std::cout << "(x, y, theta) = " << x << ", " << y << ", " << theta*180/M_PI << std::endl;
+        #endif
+    }
+
+    #ifdef FIND_ROBOT_DEBUG_PLOT
+      cv::imshow("findRobot", contours_img);
+      cv::waitKey(0);
+    #endif
+
+  return found;    
   }
 
   bool planPath(const Polygon& borders, const std::vector<Polygon>& obstacle_list, const std::vector<std::pair<int,Polygon>>& victim_list, const Polygon& gate, const float x, const float y, const float theta, Path& path){
